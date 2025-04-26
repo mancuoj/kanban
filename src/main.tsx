b6f4309407b35744130cea4751e8a1a113f5ac6f
mancuoj <mancuoj@gmail.com>
2025-04-27 00:42:14 +0800

    chore: init

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import { App } from '@/routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider } from 'next-themes'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import '@/index.css'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider storageKey="react-tmpl-lite-theme" attribute="class">
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<App />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </StrictMode>,
+)
